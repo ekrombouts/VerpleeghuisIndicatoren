b9b7fcaa42bd761caa7235b06d18e74d06f8f384
Eva Rombouts <ekrombouts@gmail.com>
2022-09-14 10:04:56 +0200

    fln en flr toegevoegd

--- scripts/Functies.R
+++ scripts/Functies.R
@@ -6,3 +6,14 @@ ToonVIvoorOrganisatieID <- function (ogi) {
   aap <- organisaties [organisaties$organisatie_ID==16, "organisatie"]
   noot <- df[df$organisatie == aap$organisatie , ]
 }
+
+# Function to list variable names of dataframe to new lines, comma seperated
+fln <- function (df) {
+  write_clip (paste (names(df), collapse = ",\n"))
+}
+
+# Print column names to console and return df
+flr <- function (df) {
+  write_clip (paste (names(df), collapse = ",\n"))
+  df
+}
