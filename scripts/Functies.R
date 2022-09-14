695edfda5179bb8aed3f843a3624943e0f93c43e
Eva Rombouts <ekrombouts@gmail.com>
2022-09-14 09:05:07 +0200

    Functies naar startscript verplaatst

--- scripts/Functies.R
+++ scripts/Functies.R
@@ -6,8 +6,3 @@ ToonVIvoorOrganisatieID <- function (ogi) {
   aap <- organisaties [organisaties$organisatie_ID==16, "organisatie"]
   noot <- df[df$organisatie == aap$organisatie , ]
 }
-
-# Function to list names
-fln <- function (df) {
-  cat (names(df), sep = ", ")
-}
